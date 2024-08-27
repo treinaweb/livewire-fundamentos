d6cbbb32ae56a568ecc0486a5f2550c0f89a24be
Elton Fonseca <elton869@gmail.com>
2024-08-27 16:59:30 -0300

    06 - Conhecendo propriedades e ações

---
+++ resources/views/livewire/contador.blade.php
@@ -0,0 +1,6 @@
+<div>
+    Meu valor atual é: {{ $contador }}
+    <br />
+    <button type="button" wire:click="incrementar">Incrementar</button>
+    <button type="button" wire:click="decrementar">decrementar</button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SiteController;
+use App\Livewire\Contador;
 use App\Livewire\OlaMundo;
 use App\Livewire\Saudacao;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,5 @@ Route::get('/', [SiteController::class, 'home']);
 
 Route::get('/saudacao', Saudacao::class);
 Route::get('/ola-mundo/{mensagem}', OlaMundo::class);
+
+Route::get('/contador', Contador::class);
